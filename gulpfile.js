aef7f8c84d5a82822e3bab72d10dc89be4916fd9
seersevenpro <dev@seerseven.com>
2022-02-13 22:24:26 -0500

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,11 @@ const plumber = require('gulp-plumber');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 const cssnano = require('gulp-cssnano');
+const gulpCopy = require('gulp-copy');
+const bump = require('gulp-bump');
+const git  = require('gulp-git');
+const push = require('gulp-git-push');
+const gitignore = require('gulp-gitignore');
 
 // ESBuild Styles Move Task
 
@@ -27,6 +32,7 @@ function css() {
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(gulp.dest('theme/assets'));
 }
+
 
 function bjs() {
 	return gulp
@@ -68,19 +74,53 @@ function js() {
 		.pipe(concat('vendor.min.js'))
 		.pipe(gulp.dest('theme/assets'));
 }
+function cdn() {
+	return gulp
+		.src('theme/assets/*')
+		.pipe(plumber())
+		.pipe(rename({ prefix: 'dist-' }))
+		.pipe(gulp.dest('docs'));
+}
+function tag() {
+	return gulp
+		.src('./package.json')
+		.pipe(plumber())
+		// bump package.json version
+		.pipe(bump({type: 'patch'}))
+		// save bumped file into filesystem
+		.pipe(gulp.dest('./'))
+}
+function deploy() {
+	return gulp
+		.src('./*')
+		.pipe(plumber())
+		.pipe(gitignore())
+		// add changes
+		.pipe(git.add())
+		// commit changes
+		.pipe(git.commit('bump version'))
+		// push local changes to repository
+		.pipe(push({                      
+			repository: 'origin',
+			refspec: 'HEAD'
+		}));
+}
+
 
 // Watch files
 function watchFiles() {
-	gulp.watch('src/build/*.css', pcss);
-	gulp.watch('src/styles/**/*', css);
 	gulp.watch('src/build/*.js', bjs);
 	gulp.watch('src/js/vendors/*.js', vjs);
 	gulp.watch('src/scripts/**/*', js);
+	gulp.watch('src/build/*.css', pcss);
+	gulp.watch('src/styles/**/*', css);
+	gulp.watch('theme/assets/**/*', cdn, tag, deploy);
 }
 
 // define complex tasks
 const build = gulp.parallel(pcss, css, bjs, vjs, js);
 const watch = gulp.series(watchFiles);
+
 
 // export tasks
 exports.pcss = pcss;
@@ -88,6 +128,9 @@ exports.css = css;
 exports.bjs = bjs;
 exports.vjs = vjs;
 exports.js = js;
+exports.cdn = cdn;
+exports.tag = tag;
+exports.deploy = deploy;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;

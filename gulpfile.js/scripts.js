fdabd962c319f14d56e8bcce3d2a18ac6a32eef6
seerseven <dev@seerseven.com>
2022-02-16 23:37:13 -0500

    bump version

--- gulpfile.js/scripts.js
+++ gulpfile.js/scripts.js
@@ -17,6 +17,7 @@ libs = libs.map((i) => scripts + i);
 function js() {
 	return src([esbuild + '*.js'])
 		.pipe(plumber())
+		.pipe(dest(dist))
 		.pipe(uglify())
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(dest(dist));

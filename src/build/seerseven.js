afbff391b7548838d407cf5fb71126fad3ec98f0
seerseven <dev@seerseven.com>
2022-02-17 04:20:32 -0500

    bump version

--- src/build/seerseven.js
+++ src/build/seerseven.js
@@ -119,4 +119,7 @@ function change(e, i, m, p) {
 // src/js/seerseven.js
 document.addEventListener("DOMContentLoaded", function() {
   change("id", "page-index", "add", "SUCK-MY-BALLS");
+  const newList = "#block-be2ef39cad6b4a7b9323e5d661b095bc";
+  const main = "#block-12f032d6c94e43738202084671106b47";
+  $(newList).appendTo(main);
 });
--- src/js/seerseven.js
+++ src/js/seerseven.js
@@ -2,4 +2,8 @@ import * as el from './utils/element';
 
 document.addEventListener('DOMContentLoaded', function () {
 	el.change('id', 'page-index', 'add', 'SUCK-MY-BALLS');
+
+	const newList = '#block-be2ef39cad6b4a7b9323e5d661b095bc';
+	const main = '#block-12f032d6c94e43738202084671106b47';
+	$(newList).appendTo(main);
 });
